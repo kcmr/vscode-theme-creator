2fb0e8dc174a295fa61948c3f13c0644b82bf578
Kus Cámara <kcmr@users.noreply.github.com>
2018-02-28 00:10:55 +0100

    chore(tasks): add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass');
 const browsersync = require('browser-sync');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 const {browserslist} = require('./package');
 const argv = require('yargs').argv;
 
@@ -22,12 +23,16 @@ const files = {
 
 gulp.task('styles', () => {
   return gulp.src(files.src.styles)
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
     .pipe(postcss([ 
       autoprefixer({ 
         browsers: browserslist[ENV] 
       }) 
     ]))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(files.dest.styles))
     .pipe(browsersync.reload({
       stream: true
