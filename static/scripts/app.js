adf79dc17e5d56e1a6d1ac79d8c187246900d3f4
Kus Cámara <kcmr@users.noreply.github.com>
2018-03-08 21:45:30 +0100

    feat(form): reset colors on reset

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,18 +1,28 @@
 (function(d) {
   const updateCSSVars = function(e) {
     const { dataset: { cssVar }, value } = e.target;
-    document.documentElement.style.setProperty(cssVar, value);
+    d.documentElement.style.setProperty(cssVar, value);
   };
 
-  const resetStyles = function(e) {
-    document.documentElement.removeAttribute('style');
+  const handleFormReset = function(e) {
+    submitBtn.disabled = true;
+    d.documentElement.removeAttribute('style');
+    form.addEventListener('change', enableSubmit);
+  };
+
+  const enableSubmit = function() {
+    submitBtn.disabled = false;
+    form.removeEventListener('change', enableSubmit);
   };
 
   const inputs = d.querySelectorAll('input[type="color"]');
+  const form = d.querySelector('form');
+  const submitBtn = d.querySelector('button[type="submit"]');
+
   for (const input of inputs) {
     input.addEventListener('change', updateCSSVars);
   }
 
-  const form = d.querySelector('form');
-  form.addEventListener('reset', resetStyles);
+  form.addEventListener('reset', handleFormReset);
+  form.addEventListener('change', enableSubmit);
 }(document));
